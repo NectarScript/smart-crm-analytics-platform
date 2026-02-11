198d554a2088f4d65de0a8459f24fd0d91c99492
NectarScript <piyushchiwande19@gmail.com>
2026-02-11 20:17:57 +0530

    Week 1: Spring Boot backend running with test API

---
+++ backend/src/main/java/com/smartcrm/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.smartcrm.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Backend running";
+    }
+}
